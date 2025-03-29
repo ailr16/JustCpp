817fdd7e26e7583a8ecefbe18c03a586f73b308c
ailr16 <angel_ivan_5@outlook.com>
2025-03-29 00:49:49 -0600

    Added exercise 1 Circle class calculator

--- exercises/src/oopExercises.cpp
+++ exercises/src/oopExercises.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "oopExercises.hpp"
+
 
 int main(void) {
-    std::cout << "This is a test!" << std::endl;
+    {
+        std::cout << "Exercise 1. Circle Class ------------" << std::endl;
+        Circle circleTest;
+
+        circleTest.setRadius(16);
+
+        std::cout << "Area: " << circleTest.computeArea() << std::endl
+                  << "Perimeter: " << circleTest.computePerimeter() << std::endl;
+
+        std::cout << "-------------------------------------" << std::endl;
+    }
+    
     return 0;
 }
